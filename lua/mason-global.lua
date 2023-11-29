9c0a57d0e64992f56c57b9f3d41dee061e8b79bb
yofreOrmaza <ormaza.main@gmail.com>
2023-11-29 15:43:23 -0500

    Mover directorio de mason a carpeta lua

--- lua/mason-global.lua
+++ lua/mason-global.lua
@@ -1,5 +1,7 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = { 'clangd', 'clangd-format',},
+    ensure_installed = {
+       'clangd',
+    },
     automatic_installation = true,
 })
