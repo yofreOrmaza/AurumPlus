72c914161fc37f8dadf77bd3e05be09f10397c1e
yofreOrmaza <ormaza.main@gmail.com>
2023-11-29 16:22:23 -0500

    style(diagnosticos): Reemplazar letras por iconos

--- lua/t-trouble.lua
+++ lua/t-trouble.lua
@@ -4,8 +4,8 @@ require("trouble").setup{
     width = 50,
     icons = true,
     mode = "document_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-    fold_open = "v", -- icon used for open folds
-    fold_closed = ">", -- icon used for closed folds
+    fold_open = "", -- icon used for open folds
+    fold_closed = "", -- icon used for closed folds
     group = true, -- group results by file
     padding = true, -- add an extra new line on top of the list
     action_keys = {
@@ -35,11 +35,11 @@ require("trouble").setup{
     auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
     signs = {
         -- icons / text used for a diagnostic
-        error = "error",
-        warning = "warn",
-        hint = "hint",
-        information = "info",
-        other = " "
+        error = "",
+        warning = "",
+        hint = "",
+        information = "",
+        other = ""
     },
     use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
 }
